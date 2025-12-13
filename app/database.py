8e32331ac0bc42fdb1ad8da45f17bd1b9f6ab884
shayanghnb <hexshayan@gmail.com>
2025-12-13 12:13:42 +0330

    models created and database connected

---
+++ app/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base
+
+DATABASE_URL = 'postgresql://postgres:1234567890@localhost:5432/restaurant managment'
+
+engine = create_engine(DATABASE_URL)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base.metadata.create_all(engine)
---
+++ app/models.py
@@ -0,0 +1,55 @@
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey, TIMESTAMP
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from datetime import datetime
+
+Base = declarative_base()
+
+class MenuItem(Base):
+    __tablename__ = 'menu_items'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    price = Column(Float, nullable=False)
+
+    order_details = relationship("OrderDetails", back_populates="menu_item")
+    def __repr__(self):
+        return f"<Menu Item(name={self.name}, price={self.price})>"
+
+
+class Table(Base):
+    __tablename__ = 'tables'
+    id = Column(Integer, primary_key=True)
+    table_number = Column(Integer)
+    status = Column(Boolean, default=True)
+
+    orders = relationship("Order",  back_populates="table")
+
+    def __repr__(self):
+        return f"table {self.id}, table number: {self.table_number}, status: {self.status}"
+
+
+class Order(Base):
+    __tablename__ = 'orders'
+    id = Column(Integer, primary_key=True)
+    table_id = Column(Integer, ForeignKey("tables.id"))
+    order_time = Column(TIMESTAMP, nullable=False, default=datetime.now())
+    status = Column(String, nullable=True)
+
+    table = relationship("Table", back_populates="orders")
+    order_details = relationship("OrderDetails", back_populates="order", cascade="all, delete-orphan")
+
+    def __repr__(self):
+        order_time_str = self.order_time.strftime("%Y-%m-%d %H:%M:%S")
+        return f"order id:{self.id}, table number:{self.table.table_number}, order time:{order_time_str}"
+
+
+class OrderDetails(Base):
+    __tablename__ = 'order_details'
+    id = Column(Integer, primary_key=True)
+    order_id = Column(Integer, ForeignKey("orders.id"))
+    item_id = Column(Integer, ForeignKey("menu_items.id"))
+    quantity = Column(Integer)
+
+    order = relationship("Order", back_populates="order_details")
+    menu_item = relationship("MenuItem", back_populates="order_details")
